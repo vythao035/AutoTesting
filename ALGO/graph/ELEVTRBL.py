aa295ec9fb25dfe714ced0b8bb392f947507d759
LGE\thao2.nguyen <thao2.nguyen@lge.com>
2023-08-17 18:20:09 +0700

    update graph practices

--- ALGO/graph/ELEVTRBL.py
+++ ALGO/graph/ELEVTRBL.py
@@ -1,30 +1,26 @@
 # your code goes here
 if __name__ == '__main__':
-    queue = []
     input_str = input().split(" ")
     [f, s, g, u, d] = [int(i) for i in input_str]
-    visited = [0 for i in range(f+1)]
+    visited = [0 for i in range(f + 1)]
+    queue = []
 
-    if s == g:
-        print(0)
-    else:
-        visited[s] = 1
-        queue.append(s)
+    visited[s] = 0
+    queue.append(s)
 
-        while len(queue) > 0:
-            cur_f = queue.pop(0)
+    while queue:
+        cur_f = queue.pop(0)
 
-            if cur_f == g:
-                print(visited[g] - 1)
+        if cur_f == g:
+            print(visited[cur_f])
+            break
+        if cur_f + u <= f and visited[cur_f + u] == 0:
+            visited[cur_f + u] = visited[cur_f] + 1
+            queue.append(cur_f + u)
 
-            else:
-                if cur_f + u <= f and visited[cur_f + u] == 0:
-                    visited[cur_f + u] = visited[cur_f] + 1
-                    queue.append(cur_f + u)
-
-                if cur_f - d >= 1 and visited[cur_f - d] == 0:
-                    visited[cur_f - d] = visited[cur_f] + 1
-                    queue.append(cur_f - d)
+        if cur_f - d >= 1 and visited[cur_f - d] == 0:
+            visited[cur_f - d] = visited[cur_f] + 1
+            queue.append(cur_f - d)
 
     if visited[g] == 0:
         print("use the stairs")
